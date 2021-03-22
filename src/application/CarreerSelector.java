71ff725ca4cb0aa6d6d195458ba12f261622fce4
Dani Ronda Palasi <48550350+jefatura2000@users.noreply.github.com>
2021-03-22 19:50:42 +0100

    spec16

--- src/application/CarreerSelector.java
+++ src/application/CarreerSelector.java
@@ -1,31 +1,25 @@
 package application;
-	
+
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
-import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
 
+public class CarreerSelector extends Application{
 
-public class Main extends Application {
-	public static Stage stage;
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage arg0) throws Exception {
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			stage=primaryStage;
-			primaryStage.show();
+			//primaryStage.setScene(scene);
+			//primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+		
 	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+
 }
